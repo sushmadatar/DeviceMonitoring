3c0e568c54c45dc78559e216bed73475a89ff407
sushmadatar <sushma.datar@gmail.com>
2019-08-22 01:12:45 +0530

    basic folder structure of server.

---
+++ DeviceMonitoringServer/src/main/java/com/example/devicemonitor/DevicemonitorApplication.java
@@ -0,0 +1,17 @@
+package com.example.devicemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DevicemonitorApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicemonitorApplication.class, args);
+	}
+
+}
